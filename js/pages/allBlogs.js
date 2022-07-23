3cdf09d7458291a580932f1c58caf94a94a13389
Roman Nadopta <42607983+RomanNadopta@users.noreply.github.com>
2022-07-23 09:34:31 +0300

    finish

--- js/pages/allBlogs.js
+++ js/pages/allBlogs.js
@@ -48,7 +48,7 @@ window.addEventListener("DOMContentLoaded", async function () {
   const animation = () => {
     AOS.init({
       delay: 200, // values from 0 to 3000, with step 50ms
-      duration: 1500, // values from 0 to 3000, with step 50ms
+      duration: 1000, // values from 0 to 3000, with step 50ms
       once: false, // whether animation should happen only once - while scrolling down
       mirror: false, // whether elements should animate out while scrolling past them
       anchorPlacement: "top-bottom", // de
